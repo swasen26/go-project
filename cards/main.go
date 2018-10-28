572d17836660843c5608fdd65d42b7a182b1e504
Swapnil Sen <swasen@cisco.com>
2018-10-28 19:41:28 +0530

    Replaced local function with newDeck for getting all cards of a deck

--- cards/main.go
+++ cards/main.go
@@ -1,28 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
-	card := newCard()
+	cards := newDeck()
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards.print()
 
-	cardsDeck := deck{"Ace of Diamonds", newCard()}
-
-	cards = append(cards, card)
-
-	fmt.Println(card)
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	cardsDeck.print()
-
-}
-
-func newCard() string {
-
-	return "Five of Diamonds"
 }
